c77802b33c386c237a394f5b5b29c1b4e0638203
ben-norton <michaelnorton.ben@gmail.com>
2025-04-15 11:03:41 -0400

    Documentation Update

--- docs/static/custom/js/app-script.js
+++ docs/static/custom/js/app-script.js
@@ -3,29 +3,26 @@
 
 $(document).ready(function () {
 	// Help popovers for SKOS terms
+	console.log("Document Ready");
 	$('[data-bs-toggle="popover"]').popover({
 		trigger: 'click',
-		container: '.popover-container',
+		container: 'body',
 		placement: 'top',
 		customClass: 'help-popover',
 		html: true
 	});
 	$('[data-bs-toggle="popover"]').click(function (e) {
+		console.log("Popover Click");
 		e.stopPropagation();
 	});
 	$('a.poplink').click(function (e) {
+		console.log("Poplink Click");
 		e.preventDefault();
 	});
 	$(document).click(function (e) {
 		if (($('.popover').has(e.target).length == 0) || $(e.target).is('.close')) {
 			$('[data-bs-toggle="popover"]').popover('hide');
 		}
-	});
-
-	// Initialize Mermaid E-R Diagrams
-	mermaid.initialize({
-		theme: 'forest',
-		securityLevel: 'loose',
 	});
 
 });
